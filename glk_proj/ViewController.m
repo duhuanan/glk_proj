04f0e03933146d4ee4dbfd69f1ae20bdd4d770f9
apple <apple@appletekiMac-mini.local>
2014-06-02 01:53:40 +0800

    Initial Commit

---
+++ glk_proj/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  glk_proj
+//
+//  Created by apple on 14-6-2.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ glk_proj/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  glk_proj
+//
+//  Created by apple on 14-6-2.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
