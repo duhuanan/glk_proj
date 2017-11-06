6426b6c7188a320dc583fcc4ab13bd179cd75793
duhuanan <547672630@qq.com>
2017-11-07 03:08:40 +0800

    1234
    请问去玩儿

--- glk_proj/AppDelegate.m
+++ glk_proj/AppDelegate.m
@@ -13,7 +13,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
-    NSLog(@"12312");
+    NSLog(@"123123241234");
     return YES;
 }
 							
